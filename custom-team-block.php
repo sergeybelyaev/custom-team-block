dc6c8385d27346aef2b0f35a1e074a4fdb379e9b
Sergey Belyaev <sbelyaev@DESKTOP-KGT.W3>
2019-06-17 12:51:58 +0300

    team_member_order_number meta_query fix

--- custom-team-block.php
+++ custom-team-block.php
@@ -13,7 +13,6 @@ add_action( 'plugins_loaded', 'custom_team_block_load_plugin_textdomain' );
 
 function custom_team_block_init() {
 
-    // Team post type
 	$labels = array(
 		'name' => __( 'Team', 'custom-team-block' ),
 		'singular_name' => __( 'Team Member', 'custom-team-block' ),
@@ -80,7 +79,23 @@ add_action( 'init', 'custom_team_block_init' );
 function custom_team_block_render_block_team( $attributes, $content ) {
 	$team_posts = get_posts( array(
 		'post_type' => 'team',
+		'post_status' => 'publish',
 		'posts_per_page' => -1,
+		'order' => 'ASC',
+		'orderby' => 'meta_value_num',
+		'meta_key' => 'team_member_order_number',
+		'meta_query' => array(
+			'relation' => 'OR',
+			array(
+				'key'=>'team_member_order_number',
+				'compare' => 'EXISTS',       
+			),
+			array( 
+				'key'=>'team_member_order_number',
+				'compare' => 'NOT EXISTS',
+			)
+		),
+		'post_per_page'=>-1
 	) );
 	ob_start();
 	if ( $team_posts ) : ?>
@@ -101,6 +116,11 @@ function custom_team_block_render_block_team( $attributes, $content ) {
 								<h4><?php echo esc_html( $team_member_postition ); ?></h4>
 							<?php endif; ?>
 							<?php echo wpautop( $p->post_content ); ?>
+							<?php if ( $team_member_linkedin = get_post_meta( $p->ID, 'team_member_linkedin', true ) ) : ?>
+								<a href="<?php echo esc_url( $team_member_linkedin ); ?>" target="_blank">
+									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"/></svg>
+								</a>
+							<?php endif; ?>
 						</div>
 					<?php endforeach; ?>
 				</div>
@@ -114,8 +134,8 @@ function custom_team_block_render_block_team( $attributes, $content ) {
 
 function custom_team_block_add_team_metaboxes() {
 	add_meta_box(
-		'team_member_postition',
-		'Team member postition',
+		'team_member_fields',
+		__( 'Team member fields', 'custom-team-block' ),
 		'custom_team_block_team_member_metaboxes_html',
 		'team',
 		'side',
@@ -127,46 +147,38 @@ add_action( 'add_meta_boxes', 'custom_team_block_add_team_metaboxes' );
 
 function custom_team_block_team_member_metaboxes_html() {
 	global $post;
-	// Nonce field to validate form request came from current site
 	wp_nonce_field( basename( __FILE__ ), 'custom_team_block_metaboxes' );
-	// Get the location data if it's already been entered
 	$team_member_postition = get_post_meta( $post->ID, 'team_member_postition', true );
-	// Output the field
-	echo '<input type="text" name="team_member_postition" value="' . esc_attr( $team_member_postition )  . '" class="widefat">';
+	echo '<p><label>' . __( 'Team member position', 'custom-team-block' );
+	echo '<input type="text" name="team_member_postition" value="' . esc_attr( $team_member_postition )  . '" class="widefat"></label></p>';
+	$team_member_order_number = get_post_meta( $post->ID, 'team_member_order_number', true );
+	echo '<p><label>' . __( 'Team member order number', 'custom-team-block' );
+	echo '<input type="number" name="team_member_order_number" value="' . esc_attr( $team_member_order_number )  . '" class="widefat" min="1"></label></p>';
+	$team_member_linkedin = get_post_meta( $post->ID, 'team_member_linkedin', true );
+	echo '<p><label>' . __( 'Team member LinkedIn', 'custom-team-block' );
+	echo '<input type="url" name="team_member_linkedin" value="' . esc_attr( $team_member_linkedin )  . '" class="widefat"></label></p>';
 }
 
-/**
- * Save the metabox data
- */
 function custom_team_block_team_member_save_meta( $post_id, $post ) {
-	// Return if the user doesn't have edit permissions.
 	if ( ! current_user_can( 'edit_post', $post_id ) ) {
 		return $post_id;
 	}
-	// Verify this came from the our screen and with proper authorization,
-	// because save_post can be triggered at other times.
-	if ( ! isset( $_POST['team_member_postition'] ) || ! wp_verify_nonce( $_POST['custom_team_block_metaboxes'], basename(__FILE__) ) ) {
+	if ( ! ( isset( $_POST['team_member_postition'] ) || isset( $_POST['team_member_order_number'] ) ) || ! wp_verify_nonce( $_POST['custom_team_block_metaboxes'], basename(__FILE__) ) ) {
 		return $post_id;
 	}
-	// Now that we're authenticated, time to save the data.
-	// This sanitizes the data from the field and saves it into an array $team_member_meta.
 	$team_member_meta['team_member_postition'] = sanitize_text_field( $_POST['team_member_postition'] );
-	// Cycle through the $team_member_meta array.
-	// Note, in this example we just have one item, but this is helpful if you have multiple.
+	$team_member_meta['team_member_order_number'] = intval( $_POST['team_member_order_number'] );
+	$team_member_meta['team_member_linkedin'] = esc_url_raw( $_POST['team_member_linkedin'] );
 	foreach ( $team_member_meta as $key => $value ) :
-		// Don't store custom data twice
 		if ( 'revision' === $post->post_type ) {
 			return;
 		}
 		if ( get_post_meta( $post_id, $key, false ) ) {
-			// If the custom field already has a value, update it.
 			update_post_meta( $post_id, $key, $value );
 		} else {
-			// If the custom field doesn't have a value, add it.
 			add_post_meta( $post_id, $key, $value);
 		}
 		if ( ! $value ) {
-			// Delete the meta key if there's no value
 			delete_post_meta( $post_id, $key );
 		}
 	endforeach;
